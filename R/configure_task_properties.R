a2b3a84f8e9aef854848895b21201ee471fe3113
Jon Harmon <jonthegeek@gmail.com>
2025-12-05 06:44:31 -0600

    Make `setup_cron.R` much more robust.

---
+++ R/configure_task_properties.R
@@ -0,0 +1,28 @@
+configure_task_properties <- function(task_name) {
+  ps_script_content <- glue::glue("
+    $taskName = '{task_name}';
+    try {{
+      $task = Get-ScheduledTask -TaskName $taskName -ErrorAction Stop;
+      $newPrincipal = New-ScheduledTaskPrincipal -UserId $task.Principal.UserId -LogonType S4U;
+      $settings = $task.Settings;
+      $settings.Hidden = $true;
+      $settings.WakeToRun = $true;
+      $settings.RunOnlyIfNetworkAvailable = $true;
+      $settings.StartWhenAvailable = $true;
+      $settings.ExecutionTimeLimit = 'PT1H'; # ISO 8601 format
+      Set-ScheduledTask -TaskName $taskName -Principal $newPrincipal -Settings $settings -ErrorAction Stop;
+    }} catch {{
+      Write-Error $_.Exception.Message;
+      Read-Host 'Press Enter to exit'; # Keep window open if it errors
+    }}
+  ")
+  
+  ps_file <- tempfile(fileext = ".ps1")
+  writeLines(ps_script_content, ps_file)
+  
+  run_command <- glue::glue(
+    "Start-Process powershell -Verb RunAs -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"{ps_file}\"'"
+  )
+  system2("powershell", args = c("-Command", run_command))
+  cli::cli_inform("Task {.str {task_name}} properties have been configured.")
+}
--- inst/setup_cron.R
+++ inst/setup_cron.R
@@ -1,59 +1,40 @@
-library(taskscheduleR)
+pkgload::load_all()
 
-taskscheduler_delete(taskname = "dslc_clubs")
-taskscheduler_delete(taskname = "dslc_clear_reminders")
-
-clubs_script <- system.file("runners", "clubs.R", package = "bookclubcron")
-start_time <- lubridate::now()
-lubridate::minute(start_time) <- 30
-if (start_time <= lubridate::now()) {
-  start_time <- start_time + lubridate::hours(1)
-}
-start_time <- paste0(
-  stringr::str_pad(lubridate::hour(start_time), 2, pad = "0"),
-  ":",
-  30
-)
-taskscheduler_create(
-  taskname = "dslc_clubs",
-  rscript = clubs_script,
+create_task <- purrr::partial(
+  taskscheduleR::taskscheduler_create, 
+  Rexe = utils::shortPathName(
+    file.path(Sys.getenv("R_HOME"), "bin", "Rscript.exe")
+  ),
   schedule = "HOURLY",
-  # schedule = "ONCE",
-  starttime = start_time,
   startdate = format(Sys.Date(), "%m/%d/%Y")
 )
 
-
-tasks <- taskscheduler_ls() |>
-  tibble::as_tibble()
-
-tasks |>
-  dplyr::filter(TaskName == "dslc_clubs") |>
-  dplyr::glimpse() |>
-  dplyr::pull("Task To Run")
-
-
-reminder_script <- system.file(
-  "runners",
-  "clear_reminders.R",
-  package = "bookclubcron"
+tasks <- tibble::tibble(
+  task = c("dslc_clubs", "dslc_clear_reminders"),
+  script = c("clubs.R", "clear_reminders.R"),
+  start_time_minutes = c(30, 0)
 )
 
-start_time <- lubridate::now()
-lubridate::minute(start_time) <- 0
-if (start_time <= lubridate::now()) {
-  start_time <- start_time + lubridate::hours(1)
-}
-start_time <- paste0(
-  stringr::str_pad(lubridate::hour(start_time), 2, pad = "0"),
-  ":",
-  "00"
-)
-taskscheduler_create(
-  taskname = "dslc_clear_reminders",
-  rscript = reminder_script,
-  schedule = "HOURLY",
-  # schedule = "ONCE",
-  starttime = start_time,
-  startdate = format(Sys.Date(), "%m/%d/%Y")
+purrr::pwalk(
+  tasks,
+  function(task, script, start_time_minutes) {
+    script_path <- system.file("runners", script, package = "bookclubcron")
+    start_time <- lubridate::now()
+    lubridate::minute(start_time) <- start_time_minutes
+    if (start_time <= lubridate::now()) {
+      start_time <- start_time + lubridate::hours(1)
+    }
+    start_time_formatted <- paste0(
+      stringr::str_pad(lubridate::hour(start_time), 2, pad = "0"),
+      ":",
+      stringr::str_pad(start_time_minutes, 2, pad = "0")
+    )
+    taskscheduleR::taskscheduler_delete(task)
+    create_task(
+      taskname = task,
+      rscript = script_path,
+      starttime = start_time_formatted
+    )
+    configure_task_properties(task)
+  }
 )
